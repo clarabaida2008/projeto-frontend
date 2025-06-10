bc4feddd9c8e9f16a517d25201b4516e1b3de516
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-06-10 10:40:46 -0300

    Código

--- src/Container.tsx
+++ src/Container.tsx
@@ -3,14 +3,16 @@ interface ContainerProps{
   nome:string
 }
 function Container(props:ContainerProps){
-  function f(){
-
+  function trataInput(event:React.ChangeEvent<HTMLInputElement>){
+    console.log(event.currentTarget.value)
   }
   return(
     <>
       <h1>{props.nome}</h1>
       Texto:{texto}
-      <input type="text" placeholder="Mostrar Texto" onChange={f}/>
+      <input type="text" 
+              placeholder="Mostrar Texto" 
+              onChange={trataInput}/>
     </>
   )
 }
