852a21a2d1ef5ddafeddfc24e434666541c98717
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-06-10 11:09:47 -0300

    Código do useState

--- src/Container.tsx
+++ src/Container.tsx
@@ -2,14 +2,18 @@ import { useState } from "react"
 interface ContainerProps{
   nome:string
 }
+
 function Container(props:ContainerProps){
+  const [texto,setTexto] = useState("")
+
   function trataInput(event:React.ChangeEvent<HTMLInputElement>){
     console.log(event.currentTarget.value)
+    setTexto(event.currentTarget.value)
   }
   return(
     <>
       <h1>{props.nome}</h1>
-      Texto:{texto}
+      <p>Texto:{texto}</p>
       <input type="text" 
               placeholder="Mostrar Texto" 
               onChange={trataInput}/>
