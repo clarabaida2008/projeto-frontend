a5dcd9515603753a967ca82f4cced6c32b3deaf9
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-06-16 11:10:28 -0300

    Código de mostrar produtos que estão no useState.

---
+++ src/Pagina.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react"
+interface ProdutosState {
+    id:number,
+    nome: string,
+    preco:number,
+    categoria:string
+}
+
+function Pagina(){
+    const [produtos,setProdutos] = useState<ProdutosState[]>([
+        {
+            id:1,
+            nome:"Caderno",
+            preco:20,
+            categoria:"Escolar"
+        }
+    ])
+    return(
+        <>
+            <header>
+                <div>Logo</div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <div className="container-listagem">
+                {produtos.map(produto=>{
+                    return(
+                        <div className="produto-container">
+                            <div className="produto-nome">
+                                {produto.nome}
+                            </div>
+                            <div className="produto-preco">
+                                {produto.preco}
+                            </div>
+                            <div className="produto-categoria">
+                                {produto.categoria}
+                            </div>
+                        </div>
+                    )
+                })}
+                </div>
+                <div className="container-cadastro">
+                    <input type="text" name="id" id="id" />
+                    <input type="text" name="nome" id="nome" />
+                    <input type="text" name="preco" id="preco"/>
+                    <input type="text" name="categoria" id="categoria"/>
+                    <input type="submit" value="Cadastrar"/>
+                </div>
+            </main>
+            <footer></footer>
+        </>
+    )
+}
+
+export default Pagina
