ccc35c5a77c4a3c101321ff351e0f7ffaa4452c4
Guilherme Figueiredo Terenciani <guilhermeterenciani@gmail.com>
2025-06-17 11:16:26 -0300

    Código de aula

--- src/Pagina.tsx
+++ src/Pagina.tsx
@@ -1,22 +1,50 @@
 import { useState } from "react"
 interface ProdutosState {
-    id:number,
+    id: number,
     nome: string,
-    preco:number,
-    categoria:string
+    preco: number,
+    categoria: string
 }
 
-function Pagina(){
-    const [produtos,setProdutos] = useState<ProdutosState[]>([
+function Pagina() {
+    const [id,setId] = useState("")
+    const [nome,setNome] = useState("")
+    const [preco,setPreco] = useState("")
+    const [categoria,setCategoria] = useState("")
+    const [produtos, setProdutos] = useState<ProdutosState[]>([
         {
-            id:1,
-            nome:"Caderno",
-            preco:20,
-            categoria:"Escolar"
+            id: 1,
+            nome: "Caderno",
+            preco: 20,
+            categoria: "Escolar"
         }
     ])
-    function TrataCadastro(){}
-    return(
+    function TrataCadastro(event:React.FormEvent<HTMLFormElement>){
+        event.preventDefault();
+        //Criar um novo produto
+        const novoProduto:ProdutosState = {
+            id:parseInt(id),
+            nome:nome,
+            preco:parseFloat(preco),
+            categoria:categoria
+        }
+        //Adicionar esse novo produto no vetor/Array de produtos
+        setProdutos([...produtos,novoProduto])
+        
+    }
+    function trataId(event:React.ChangeEvent<HTMLInputElement>){
+        setId(event.target.value)
+    }
+    function trataNome(event:React.ChangeEvent<HTMLInputElement>){
+        setNome(event.target.value)
+    }
+    function trataPreco(event:React.ChangeEvent<HTMLInputElement>){
+        setPreco(event.target.value)
+    }
+    function trataCategoria(event:React.ChangeEvent<HTMLInputElement>){
+        setCategoria(event.target.value)
+    }
+    return (
         <>
             <header>
                 <div>Logo</div>
@@ -36,28 +64,31 @@ function Pagina(){
             </header>
             <main>
                 <div className="container-listagem">
-                {produtos.map(produto=>{
-                    return(
-                        <div className="produto-container">
-                            <div className="produto-nome">
-                                {produto.nome}
+                    {produtos.map(produto => {
+                        return (
+                            <div className="produto-container">
+                                <div className="produto-nome">
+                                    {produto.nome}
+                                </div>
+                                <div className="produto-preco">
+                                    {produto.preco}
+                                </div>
+                                <div className="produto-categoria">
+                                    {produto.categoria}
+                                </div>
                             </div>
-                            <div className="produto-preco">
-                                {produto.preco}
-                            </div>
-                            <div className="produto-categoria">
-                                {produto.categoria}
-                            </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
                 </div>
                 <div className="container-cadastro">
-                    <input type="text" name="id" id="id" />
-                    <input type="text" name="nome" id="nome" />
-                    <input type="text" name="preco" id="preco"/>
-                    <input type="text" name="categoria" id="categoria"/>
-                    <input type="submit" value="Cadastrar" onClick={TrataCadastro}/>
+                    <form onSubmit={TrataCadastro}>
+                        <input type="text" name="id" id="id" onChange={trataId}/>
+                        <input type="text" name="nome" id="nome" onChange={trataNome} />
+                        <input type="text" name="preco" id="preco" onChange={trataPreco} />
+                        <input type="text" name="categoria" id="categoria" onChange={trataCategoria} />
+                        <input type="submit" value="Cadastrar"/>
+                    </form>
+
                 </div>
             </main>
             <footer></footer>
